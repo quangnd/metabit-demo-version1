32739136e5f84fba46b48d8cce8bea7a9e8932f6
quangnd <quangnd.edu@gmail.com>
2016-11-29 17:16:37 +0700

    Add draft.js and a demo layout

--- app/components/Admin/PersonalityManage.js
+++ app/components/Admin/PersonalityManage.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import SideBar from './SideBar';
-
+import {Editor, EditorState} from 'draft-js';
 
 class PersonalityManage extends React.Component {
     constructor() {
         super();
 
-        this.state = {
-            editorState: ''
-        }
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
 
-        this.handlerChange = this.handlerChange.bind(this);
+        //this.handlerChange = this.handlerChange.bind(this);
     }
 
-    handlerChange() {
-        this.setState({
-            editorState: 'abcxyz'
-        })
-    }
+
+
     render() {
+          
+          const {editorState} = this.state;
         return (
             <div id="adminDashboard">
                 <div className="navbar-default sidebar" role="navigation">
@@ -29,7 +27,9 @@ class PersonalityManage extends React.Component {
                             <div className="col-lg-12">
                                 <h1 className="page-header">Personality editor page</h1>
 
-                               
+                                <div style={styles.editor} >
+                                 <Editor editorState={editorState} onChange={this.onChange} />;
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -39,4 +39,21 @@ class PersonalityManage extends React.Component {
     }
 }
 
+ const styles = {
+        root: {
+          fontFamily: '\'Helvetica\', sans-serif',
+          padding: 20,
+          width: 600,
+        },
+        editor: {
+          border: '1px solid #ccc',
+          cursor: 'text',
+          minHeight: 80,
+          padding: 10,
+        },
+        button: {
+          marginTop: 10,
+          textAlign: 'center',
+        },
+      };
 export default PersonalityManage;
