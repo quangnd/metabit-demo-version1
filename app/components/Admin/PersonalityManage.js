67d524f5023a8074ce22664e6516a644d9f41169
quangnd <quangnd.edu@gmail.com>
2016-11-30 08:31:39 +0700

    Update Personality Manage

--- app/components/Admin/PersonalityManage.js
+++ app/components/Admin/PersonalityManage.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import SideBar from './SideBar';
-import {Editor, EditorState} from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 class PersonalityManage extends React.Component {
     constructor() {
         super();
 
-        this.state = {editorState: EditorState.createEmpty()};
-        this.onChange = (editorState) => this.setState({editorState});
+        this.state = { editorState: EditorState.createEmpty() };
+        this.onChange = (editorState) => this.setState({ editorState });
+        this.handleKeyCommand = this.handleKeyCommand.bind(this);
     }
 
-    render() {          
+    handleKeyCommand(command) {
+        const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+        if (newState) {
+            this.onChange(newState);
+            return 'handled';
+        }
+        return 'not-handled';
+    }
+
+    _onBoldClick() {
+        this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+    }
+
+    render() {
         const {editorState} = this.state;
         return (
             <div id="adminDashboard">
@@ -23,7 +37,11 @@ class PersonalityManage extends React.Component {
                                 <h1 className="page-header">Personality editor page</h1>
 
                                 <div style={styles.editor} >
-                                 <Editor editorState={editorState} onChange={this.onChange} />;
+                                    <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+                                    <Editor editorState={editorState}
+                                        onChange={this.onChange}
+                                        handleKeyCommand={this.handleKeyCommand}
+                                        />
                                 </div>
                             </div>
                         </div>
@@ -34,22 +52,22 @@ class PersonalityManage extends React.Component {
     }
 }
 
- const styles = {
-        root: {
-          fontFamily: '\'Helvetica\', sans-serif',
-          padding: 20,
-          width: 600,
-        },
-        editor: {
-          border: '1px solid #ccc',
-          cursor: 'text',
-          minHeight: 80,
-          padding: 10,
-        },
-        button: {
-          marginTop: 10,
-          textAlign: 'center',
-        },
-      };
-      
+const styles = {
+    root: {
+        fontFamily: '\'Helvetica\', sans-serif',
+        padding: 20,
+        width: 600,
+    },
+    editor: {
+        border: '1px solid #ccc',
+        cursor: 'text',
+        minHeight: 80,
+        padding: 10,
+    },
+    button: {
+        marginTop: 10,
+        textAlign: 'center',
+    },
+};
+
 export default PersonalityManage;
--- to-do-list.md
+++ to-do-list.md
@@ -1,13 +1,18 @@
-# Need to do:
+# Rich text editor:
 
-1. [DONE] ~~Add full name, email to Start Info~~
-2. [DONE] ~~Add question group to form Values to submit~~
-3. [DONE] ~~Generate userId based on timestamp.~~
-5. [DONE] ~~Remember save CreatedTime~~
+1. [ ] Init data for draft.js.
+2. [ ] Save data 
+
+# General
+
+1. [x] ~~Add full name, email to Start Info~~
+2. [x] ~~Add question group to form Values to submit~~
+3. [x] ~~Generate userId based on timestamp.~~
+5. [x] ~~Remember save CreatedTime~~
 6. At server: Validate request body (controller/quiz.js)
-7. [DONE] ~~At client: Loop all 60 question, if question is not choice, add default =0~~
+7. [x] ~~At client: Loop all 60 question, if question is not choice, add default =0~~
 8. Re-validate and layout Start Info
-9. [DONE] Re-check choose question randomly
+9. [x] Re-check choose question randomly
 10. Redirect user access Result page if not found
-11. Add admin page for view results.
+11.[x] Add admin page layout for view results.
        
